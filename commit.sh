73784b40dade051031be0c0c3bcd798817a5480c
cothdragon <huzigenta@gmail.com>
2016-06-04 12:00:00 +0800

    Run audio engine test

--- commit.sh
+++ commit.sh
@@ -6,15 +6,15 @@ for Y in {2016..2016}
 do
   mkdir $Y
   cd $Y
-  for M in {05..05}
+  for M in {06..06}
   do
     mkdir $M
     cd $M
-    for D in {5..23..9}
+    for D in {01..04}
     do
       mkdir $D
       cd $D
-      range=$((RANDOM % 4))
+      range=$((RANDOM % 5))
       for i in `eval echo {1..$range}`
       do
         echo "$i on $M/$D/$Y" > commit.md
